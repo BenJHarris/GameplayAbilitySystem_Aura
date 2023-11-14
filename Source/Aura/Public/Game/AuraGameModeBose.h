e5f4b825c3747057ec683638f5654839bc2c9cd6
Ben Harris <benjharris@hotmail.co.uk>
2023-11-14 21:57:56 +0000

    12

---
+++ Source/Aura/Public/Game/AuraGameModeBose.h
@@ -0,0 +1,17 @@
+// Copyright Ben Harris
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBose.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBose : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
