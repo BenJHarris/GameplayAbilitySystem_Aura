9a6d18d0e592441ec888ece2ea94a50443c0f4b2
Ben Harris <benjharris@hotmail.co.uk>
2023-11-15 23:46:45 +0000

    23

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -37,3 +37,9 @@ void AAuraEnemy::UnHighlightActor()
 		Weapon->SetRenderCustomDepth(false);
 	}
 }
+
+void AAuraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
