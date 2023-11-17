c4b80494118a61e709c3599ff07094452903f5c6
Ben Harris <benjharris@hotmail.co.uk>
2023-11-17 23:17:29 +0000

    29

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Ben Harris
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Ben Harris
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
