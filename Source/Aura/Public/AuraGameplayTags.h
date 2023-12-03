408a33c1c9ba7f4769ae27c5d07f61518e3f347c
Ben Harris <benjharris@hotmail.co.uk>
2023-12-03 23:57:07 +0000

    87

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,23 @@
+// Copyright Ben Harris
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * Aura Gameplay Tags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+struct FAuraGameplayTags
+{
+public:
+    static const FAuraGameplayTags& Get() { return GameplayTags; }
+    static void InitializeNativeGameplayTags();
+ 
+    FGameplayTag Attributes_Secondary_Armor;
+protected:
+private:
+    static FAuraGameplayTags GameplayTags;
+};
