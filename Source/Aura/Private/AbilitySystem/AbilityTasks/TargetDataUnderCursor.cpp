0da00c3247d213fc531149017d350189d4a5ec8a
Ben Harris <benjharris@hotmail.co.uk>
2024-01-07 20:47:11 +0000

    117

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderCursor.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderCursor.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderCursor.h"
 
+#include "AbilitySystemComponent.h"
+
 UTargetDataUnderCursor* UTargetDataUnderCursor::CreateTargetDataUnderCursor(UGameplayAbility* OwningAbility)
 {
 	UTargetDataUnderCursor* Obj = NewAbilityTask<UTargetDataUnderCursor>(OwningAbility);
@@ -12,8 +14,40 @@ UTargetDataUnderCursor* UTargetDataUnderCursor::CreateTargetDataUnderCursor(UGam
 void UTargetDataUnderCursor::Activate()
 {
 	Super::Activate();
+
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if (bIsLocallyControlled)
+	{
+		SendMouseCursorData();
+	} else
+	{
+		// TODO: We are on teh server, so listen for target data.
+	}
+}
+
+void UTargetDataUnderCursor::SendMouseCursorData()
+{
+	FScopedPredictionWindow ScopedPredictionWindow(AbilitySystemComponent.Get());
+	
 	const APlayerController* PlayerController = Ability->GetCurrentActorInfo()->PlayerController.Get();
 	FHitResult CursorHit;
 	PlayerController->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
-	ValidData.Broadcast(CursorHit.Location);
+	
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+
+	FGameplayAbilityTargetDataHandle DataHandle;
+	DataHandle.Add(Data);
+
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(),
+		GetActivationPredictionKey(),
+		DataHandle,
+		FGameplayTag(),
+		AbilitySystemComponent->ScopedPredictionKey);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
 }
