50c94fac700a516d531787033808dc19967cade2
Ben Harris <benjharris@hotmail.co.uk>
2025-01-13 23:48:17 +0000

    389

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -6,6 +6,8 @@
 #include "MVVMViewModelBase.h"
 #include "MVVM_LoadScreen.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSlotSelected);
+
 class UMVVM_LoadSlot;
 /**
  * 
@@ -21,6 +23,9 @@ public:
 	TSubclassOf<UMVVM_LoadSlot> LoadSlotViewModelClass;
 
 	void InitializeLoadSlots();
+
+	UPROPERTY(BlueprintAssignable)
+	FSlotSelected SlotSelected;
 
 	UFUNCTION(BlueprintPure)
 	UMVVM_LoadSlot* GetLoadSlotViewModelByIndex(int32 Index) const;
