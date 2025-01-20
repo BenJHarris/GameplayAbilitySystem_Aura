bb5d12d27dbf57161b990ffbfdbaadf7b8bb5dc1
Ben Harris <benjharris@hotmail.co.uk>
2025-01-20 23:45:38 +0000

    394

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -42,6 +42,9 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void DeleteButtonPressed();
 
+	UFUNCTION(BlueprintCallable)
+	void PlayButtonPressed();
+
 	void SetNumLoadSlots(int32 InNumLoadSlots);
 
 	int32 GetNumLoadSlots() const { return NumLoadSlots; }
